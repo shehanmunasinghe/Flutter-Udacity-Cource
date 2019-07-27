3c79eb19f6fb638c65efd0b1016e5d4dcc587358
Shehan Munasinghe <shehanmunasinghe@gmail.com>
2019-07-27 16:10:18 +0530

    Added converter_route

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -5,6 +5,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:udacity_cource/category.dart';
+import 'package:udacity_cource/unit.dart';
 
 final _backgroundColor = Colors.green[100];
 
@@ -50,6 +51,17 @@ class CategoryRoute extends StatelessWidget {
     );
   }
 
+  /// Returns a list of mock [Unit]s.
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final categories = <Category>[];
@@ -59,6 +71,7 @@ class CategoryRoute extends StatelessWidget {
         name: _categoryNames[i],
         color: _baseColors[i],
         iconLocation: Icons.cake,
+        units: _retrieveUnitList(_categoryNames[i]),
       ));
     }
 
