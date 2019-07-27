e1ed86f59a1b02cb7baafc0e316e05763d4d21ab
Shehan Munasinghe <shehanmunasinghe@gmail.com>
2019-07-27 15:30:14 +0530

    Added ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:udacity_cource/category.dart';
-
-// TODO: Pass this information into your custom [Category] widget
-const _categoryName = 'Cake';
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
+import 'package:udacity_cource/category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -20,17 +15,7 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Center(
-          // TODO: Determine what properties you'll need to pass into the widget
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      home: CategoryRoute(),
     );
   }
 }
